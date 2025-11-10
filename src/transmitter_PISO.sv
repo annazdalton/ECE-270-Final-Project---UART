60463f1c6a7dd2c23000c97d880a531fe4c3eb80
Anna <dalton45@purdue.edu>
2025-11-10 17:00:30 -0500

    PISO shift register logic done, outputs LSB first

--- src/transmitter_PISO.sv
+++ src/transmitter_PISO.sv
@@ -1,22 +1,36 @@
 module transmitter_PISO(
     input clk, nrst,
     input logic [7:0] data_i,
-    input logic shift_en,
+    input logic shift_en, load,
     output logic piso_o
 );
     logic nextpiso_o;
+    logic piso_data, nextpiso_data
 
     always_ff @(posedge clk, negedge nrst) begin
         if (~nrst) begin
             piso_o <= '0;
+            piso_data <= '0;
         end else begin
             piso_o <= nextpiso_o;
+            piso_data <= nextpiso_data;
         end
     end
 
+    //shift register logic, parallel load has priority 
+    //LSB gets sent out first
     always_comb begin
+        if(load) begin
+            nextpiso_data = data_i;
+            nextpiso_o = piso_o;
+        end else if (shift_en) begin
+            nextpiso_data = {1'b0, piso_data[7:1]};
+            nextpiso_o = piso_data[0];
 
+        end else begin
+            nextpiso_data = piso_data;
+            nextpiso_o = piso_o;
+        end
     end
-
 
 endmodule 
