123230249c7b50d6e1cae92268e719cce02e7ce4
Anna <dalton45@purdue.edu>
2025-11-10 16:46:01 -0500

    added ff logic for piso

--- src/transmitter_PISO.sv
+++ src/transmitter_PISO.sv
@@ -1,8 +1,22 @@
 module transmitter_PISO(
+    input clk, nrst,
     input logic [7:0] data_i,
     input logic shift_en,
     output logic piso_o
 );
+    logic nextpiso_o;
+
+    always_ff @(posedge clk, negedge nrst) begin
+        if (~nrst) begin
+            piso_o <= '0;
+        end else begin
+            piso_o <= nextpiso_o;
+        end
+    end
+
+    always_comb begin
+
+    end
 
 
 endmodule 
