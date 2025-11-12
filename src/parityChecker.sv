ccc50dcbaca1d49f1a5ce373ee601948661f9327
kmantha8 <kmantha@purdue.edu>
2025-11-12 17:32:38 -0500

    Update and rename parityCheck.sv to parityChecker.sv

--- src/parityChecker.sv
+++ src/parityChecker.sv
@@ -1,8 +1,8 @@
-module parityCheck(
+//will add parity check logic
+module parityChecker(
     input logic [7:0] sipo_i,
     input logic parity_en, parity_bit, //parity_bit is data_i from transmitter
     output logic parity_error
 );
-
 
 endmodule
