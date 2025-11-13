b2557bb7ca9e64d02cb325dcd95632fa5dc14b0e
kmantha8 <kmantha@purdue.edu>
2025-11-13 17:23:33 -0500

    Update parityChecker.sv
    Added parity checker logic

--- src/parityChecker.sv
+++ src/parityChecker.sv
@@ -1,8 +1,13 @@
-//will add parity check logic
-module parityChecker(
+//using even parity
+module parityCheck(
     input logic [7:0] sipo_i,
     input logic parity_en, parity_bit, //parity_bit is data_i from transmitter
     output logic parity_error
 );
-
+    always_comb begin
+        parity_error = 1'b0;
+        if(parity_en) begin
+            parity_error = ^{sipo_i, parity_bit};
+        end
+    end
 endmodule
