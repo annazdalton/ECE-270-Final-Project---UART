19bff50bf3145db6230255e55656166bcea6e862
Anna <dalton45@purdue.edu>
2025-11-29 17:47:49 -0500

    done with transmitter mux

--- src/transmitterMux.sv
+++ src/transmitterMux.sv
@@ -3,6 +3,16 @@ module transmitterMux(
     input logic [1:0] select,
     output logic tx_data_o
 );
-
+    always_comb begin
+        if(select = 1'b00) begin
+            tx_data_o = start;
+        end else if (select = 1'b01) begin
+            tx_data_o = piso_i;
+        end else if (select = 1'b10) begin
+            tx_data_o = parity_i;
+        end else begin //select =  11
+            tx_data_o = stop; 
+        end
+    end
 
 endmodule 
