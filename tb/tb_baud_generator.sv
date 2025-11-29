25c582b390d5eb5bc0939f93728f1fcb497777fa
Anna <dalton45@purdue.edu>
2025-11-29 18:24:51 -0500

    finished baud gen testbench

--- tb/tb_baud_generator.sv
+++ tb/tb_baud_generator.sv
@@ -2,8 +2,31 @@
 
 module tb_baud_generator();
 
+logic clk, nrst, baud_tick;
+
+baud_generator baud_test(
+    .clk(clk),
+    .nrst(nrst),
+    .baud_tick(baud_tick)
+);
+
+always begin
+    #10
+    clk = 1'b0;
+    #10
+    clk = 1'b1;
+end
 
 initial begin
+    clk = 1'b0;
+
+    //reset
+    nrst = 1'b1;
+    #2
+    nrst = 1'b0;
+    #2 
+    nrst = 1'b1;
+
     $dumpfile("baud_generator.vcd");
     $dumpvars(0, tb_baud_generator);
 end
