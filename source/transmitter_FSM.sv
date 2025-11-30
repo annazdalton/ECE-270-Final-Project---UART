793e72499b9b18f0dff74f93372237fa8202a91c
Anna Dalton <annazd06@gmail.com>
2025-11-29 23:37:09 -0500

    added load state to FSM, added shift enable and load outputs

--- source/transmitter_FSM.sv
+++ source/transmitter_FSM.sv
@@ -1,16 +1,17 @@
 module transmitter_FSM(
     input logic clk, nrst, baud_tick, tx_valid, parity_en,
     input logic [3:0] count_data,
-    output logic start, stop, count_en, count_clear,
+    output logic start, stop, count_en, count_clear, shift_en, load,
     output logic [1:0] select
 );
 
     typedef enum logic [2:0]{  
         IDLE = 0,
-        START = 1,
-        DATA = 2,
-        PARITY = 3,
-        STOP = 4
+        LOAD = 1,
+        START = 2,
+        DATA = 3,
+        PARITY = 4,
+        STOP = 5
     } state_t;
 
     state_t state, nextState;
@@ -25,7 +26,6 @@ module transmitter_FSM(
 
     always_comb begin
         //defaults to prevent latch
-        nextState = state;
         count_en = 1'b0;
         count_clear = 1'b0;
 
@@ -33,16 +33,23 @@ module transmitter_FSM(
         stop = 1'b1;
 
         select = 2'b11;
+        load = 1'b0;
+        shift_en = 1'b0;
 
         case(state) 
             IDLE: begin 
                 select = 2'b11; // stop/idle
                 
                 if(tx_valid) begin
-                    nextState = START;
+                    nextState = LOAD;
                 end else begin
                     nextState = IDLE;
                 end
+            end
+            LOAD: begin
+                load = 1'b1;
+                select = 2'b11;
+                nextState = START;
             end
             START: begin 
                 select = 2'b00; //send start bit to rx
@@ -57,19 +64,20 @@ module transmitter_FSM(
             DATA: begin 
                 //send data serially output from PISO
                 select = 2'b01;
-                count_en = baud_tick; //counter only incremnets on baud_tick
 
                 if(baud_tick) begin
+                    shift_en = 1'b1;
+                    count_en = 1'b1;
                     //if done counting
                     if(count_data == 4'd7) begin
+                        //reset counter
+                        count_en = 1'b0;
+                        count_clear = 1'b1;
                         if(parity_en) begin
                             nextState = PARITY;
                         end else begin
                             nextState = STOP;
                         end
-                        //reset counter
-                        count_en = 1'b0;
-                        count_clear = 1'b1;
                     end else begin
                         nextState = DATA; //keep sending data out
                     end
