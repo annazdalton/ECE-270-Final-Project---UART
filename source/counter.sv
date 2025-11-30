4f55eae6fb6dc41a1953fd7fd85996619f03cb2e
Anna Dalton <annazd06@gmail.com>
2025-11-30 00:00:24 -0500

    fixed counter syntax error

---
+++ source/counter.sv
@@ -0,0 +1,34 @@
+// counter, counts to 8 for transmitter FSM
+// Anna Dalton (https://github.com/annazdalton)
+
+module counter(
+    input logic clk, nrst, enable, clear,
+    output logic [3:0] count,
+    output logic overflow_flag
+);
+    logic [3:0] nextCount;
+
+    always_ff @(posedge clk, negedge ~nrst) begin
+        if(~nrst) begin
+            count <= '0;
+        end else begin
+            count <= nextCount;
+        end
+    end
+
+    always_comb begin
+        if(clear) begin
+            nextCount = '0;
+        end else if(enable) begin
+            nextCount = count + 4'd1; 
+            //overflow flag is high if count > 8
+            if(count == 4'd8) begin
+                overflow_flag = 1'b1; 
+            end else begin
+                overflow_flag = 1'b0;
+            end
+        end else begin
+            nextCount = count;
+        end
+    end
+endmodule 
