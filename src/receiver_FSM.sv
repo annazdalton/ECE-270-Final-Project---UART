b134cde0f4b52a55b71081115aa566ffa3063722
kmantha8 <kmantha@purdue.edu>
2025-12-01 12:46:43 -0500

    Update receiver_FSM.sv
    removed typedef block and renamed states as binary values

--- src/receiver_FSM.sv
+++ src/receiver_FSM.sv
@@ -4,21 +4,20 @@ module receiver_FSM(
     output logic shift_en, parity_en, frame_en,
     output logic data_ready
 );
+    // typedef enum logic [2:0]{
+    //     IDLE = 0,
+    //     START = 1,
+    //     DATA = 2,
+    //     PARITY = 3,
+    //     STOP = 4
+    // } state_t;
 
-    typedef enum logic [2:0]{
-        IDLE = 3'd0,
-        START = 3'd1,
-        DATA = 3'd2,
-        PARITY = 3'd3,
-        STOP = 3'd4
-    } state_t;
-
-    state_t state, nextState;
+    logic [2:0] state, nextState;
     logic [3:0] bit_count, nextCount;
 
-    always_ff @(posedge clk, negedge ~nrst) begin
+    always_ff @(posedge clk, negedge nrst) begin
         if(~nrst) begin
-            state <= IDLE;
+            state <= 3'd0;
             bit_count <= '0;
         end else begin  
             state <= nextState;
@@ -35,58 +34,59 @@ module receiver_FSM(
         nextCount = bit_count;
 
         case(state)
-            IDLE: begin
+            3'd0: begin //IDLE
                 nextCount = '0;
-                if (data_i == 1'b0) begin nextState = START; end
-                else begin nextState = IDLE; end
+                if (data_i == 1'b0) begin nextState = 3'd1; end
+                else begin nextState = 3'd0; end
             end
-            START: begin
+            3'd1: begin //START
                 if (baud_tick == 1'b0) begin
-                    nextState = START;
+                    nextState = 3'd1;
                 end else if (baud_tick == 1'b1 && data_i == 1'b0) begin 
-                    nextState = DATA; 
+                    nextState = 3'd2; 
                     nextCount = '0;
                 end
                 else if(baud_tick == 1'b1 && data_i == 1'b1) begin 
-                    nextState = IDLE; 
+                    nextState = 3'd0; 
                 end
             end    
-            DATA: begin
+            3'd2: begin //DATA
                 if (baud_tick == 1'b1 && bit_count == 4'd7 && parity_en_i == 1'b1) begin
                     shift_en = 1'b1;
-                    nextState = PARITY;
+                    nextState = 3'd3;
                     nextCount = '0;
                 end else if (baud_tick == 1'b1 && bit_count < 4'd7) begin
                     shift_en = 1'b1;
-                    nextState = DATA;
+                    nextState = 3'd2;
                     nextCount = bit_count + 4'd1;
                 end else if (baud_tick == 1'b1 && bit_count == 4'd7 && parity_en_i == 1'b0) begin
                     shift_en = 1'b1;
-                    nextState = STOP;
+                    nextState = 3'd4;
                     nextCount = '0;
                 end else if (baud_tick == 1'b0) begin
-                    nextState = DATA;
+                    nextState = 3'd2;
                 end
             end
-            PARITY: begin
+            3'd3: begin //PARITY
                 if (baud_tick == 1'b0) begin
-                    nextState = PARITY;
+                    nextState = 3'd3;
                 end else begin
                     parity_en = 1'b1;
-                    nextState = STOP;
+                    nextState = 3'd4;
                 end
             end
-            STOP: begin 
+            3'd4: begin //STOP
                 if (baud_tick == 1'b0) begin
-                    nextState = STOP;
+                    nextState = 3'd4;
                 end else begin
                     frame_en = 1'b1;
                     data_ready = 1'b1;
-                    nextState = IDLE;
+                    nextState = 3'd0;
                 end
             end
-            default: begin nextState = IDLE; end
+            default: begin nextState = 3'd0; end
         endcase
     end
 
 endmodule
+
