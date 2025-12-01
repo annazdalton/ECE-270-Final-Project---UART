32781fa62b494f20b89e153f5b8620b7ef447410
Anna Dalton <annazd06@gmail.com>
2025-11-30 22:37:21 -0500

    fixed timing error in PISO

--- source/transmitter_PISO.sv
+++ source/transmitter_PISO.sv
@@ -7,15 +7,12 @@ module transmitter_PISO(
     input logic shift_en, load,
     output logic piso_o
 );
-    logic nextpiso_o;
     logic [7:0] piso_data, nextpiso_data;
 
     always_ff @(posedge clk, negedge nrst) begin
         if (~nrst) begin
-            piso_o <= '0;
             piso_data <= '0;
         end else begin
-            piso_o <= nextpiso_o;
             piso_data <= nextpiso_data;
         end
     end
@@ -25,14 +22,13 @@ module transmitter_PISO(
     always_comb begin
         if(load) begin
             nextpiso_data = data_i;
-            nextpiso_o = piso_o;
         end else if (shift_en) begin
             nextpiso_data = {1'b0, piso_data[7:1]};
-            nextpiso_o = piso_data[0];
         end else begin
             nextpiso_data = piso_data;
-            nextpiso_o = piso_o;
         end
     end
+
+    assign piso_o = piso_data[0];
 
 endmodule 
